2abc4a941f5510a04efea387ee4413625dede10c
sharanyakamath <sherrykamath@gmail.com>
2018-03-04 21:55:36 +0530

    main added

--- os_sim/os_sim/settings.py
+++ os_sim/os_sim/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'main.apps.MainConfig',
     'proc_sch.apps.ProcSchConfig',
     'django.contrib.admin',
     'django.contrib.auth',

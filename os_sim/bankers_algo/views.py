e95d97aa0701df8a9761df21ab05e1bfd0f948be
Praveen Naik <praveennaik.s98@gmail.com>
2018-03-28 00:38:25 +0530

    Semaphores Added

--- os_sim/bankers_algo/views.py
+++
@@ -1,4 +0,0 @@
-from django.http import HttpResponse
-
-def index(request):
-    	return HttpResponse("<h1>Bankers Algorithm</h1>")
--- os_sim/main/views.py
+++
@@ -1,5 +0,0 @@
-from django.http import HttpResponse
-
-# Create your views here.
-def ind(request):
-	return HttpResponse("<body> <h1> OS Simulation</h1> <h3>Topics:</h3> <ul> <li>System Calls</li> <li><a href=http://127.0.0.1:8000/proc_sch>Process Scheduling </a> </li> <li>Process Synchronisation</li> <li>Socket Programming</li> <li><a href=http://127.0.0.1:8000/bankers_algo>Banker's Algorithm </a></li> <li>File Management</li> </ul> <br> <a href=http://127.0.0.1:8000/admin>Click here for simulation</a> </body>")

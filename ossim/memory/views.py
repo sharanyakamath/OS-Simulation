2f2466c020f267ffe94531ce151b0ac86b75e23c
keerthanapolkampally <keerthana.keetu.p@gmail.com>
2018-04-06 11:06:41 +0530

    Colors Added

--- ossim/memory/views.py
+++ ossim/memory/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect,HttpResponse
-from django.urls import reverse
+from django.core.urlresolvers import reverse
 from django.views.decorators.csrf import csrf_exempt
 import json
 from django.http import JsonResponse
